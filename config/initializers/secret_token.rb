c0de6dba493f807aa991065c9207bf5879e6b664
Austin Schneider <soccer022483@gmail.com>
2010-08-25 23:09:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamzu::Application.config.secret_token = '9e93db1d40021c2ba18bec8a4dbe7c7d1ad6e47f622f1de8d2bc2fc78564f09ab5aa93be57a3ddcd4c0a4ee2c83b6cbd0b5aec197e0964fd3b648b35470d79ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Teamzu::Application.config.session_store :cookie_store, :key => '_teamzu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Teamzu::Application.config.session_store :active_record_store
